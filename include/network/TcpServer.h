6138d12400a0f5743ee02a13d2f606a6793f01fe
Armandas-K <armandas2005@gmail.com>
2025-11-05 23:53:26 +0000

    add class tcpServer header for accepting and handling client requests

--- include/network/TcpServer.h
+++ include/network/TcpServer.h
@@ -1,9 +1,20 @@
 #ifndef ENCRYPTEDMESSENGER_TCPSERVER_H
 #define ENCRYPTEDMESSENGER_TCPSERVER_H
 
+#include <asio.hpp>
+#include <memory>
+#include "network/tcpConnection.h"
 
 class TcpServer {
-};
+public:
+    TcpServer(asio::io_context& io_context, unsigned short port);
 
+    void startAccept();
+
+private:
+    void handleAccept(TcpConnection::pointer new_connection, const std::error_code& error);
+
+    asio::ip::tcp::acceptor acceptor_;
+};
 
 #endif //ENCRYPTEDMESSENGER_TCPSERVER_H
