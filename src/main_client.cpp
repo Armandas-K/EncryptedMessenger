40ebce362cbe3434d61f4333d776d274702f1d2d
Armandas-K <armandas2005@gmail.com>
2025-11-09 02:06:37 +0000

    fix client constructor in main_client

--- src/main_client.cpp
+++ src/main_client.cpp
@@ -2,8 +2,12 @@
 #include "client/Client.h"
 
 int main() {
-    auto client = std::make_shared<Client>();
+    asio::io_context io_context;
+    auto connection = TcpConnection::create(io_context, nullptr);
+    auto client = std::make_shared<Client>(connection);
+
     CLI cli(client);
     cli.run();
+
     return 0;
 }
