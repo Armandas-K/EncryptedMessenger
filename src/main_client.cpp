9767311e29245ad1f9e02284e4d3e9787a3e9b63
Armandas-K <armandas2005@gmail.com>
2025-12-28 23:54:58 +0000

    move server from main_client to seperate exec main_server

--- src/main_client.cpp
+++ src/main_client.cpp
@@ -1,42 +1,27 @@
+#include "iostream"
 #include "client/CLI.h"
 #include "client/Client.h"
-#include "network/tcpServer.h"
 
 int main() {
-    // server (prob shouldnt be in client)
-    asio::io_context serverIo;
-    auto serverWork =
-        asio::make_work_guard(serverIo);
-
-    std::thread serverThread([&]() {
-        TcpServer server(serverIo, 5555);
-        serverIo.run();
-    });
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(200));
-
-    // client
-    asio::io_context io;
-    auto clientWork =
-        asio::make_work_guard(io);
-
-    std::thread ioThread([&]() {
-        io.run();
-    });
-
     try {
+        asio::io_context io;
+
+        // asio background thread
+        std::thread ioThread([&]() {
+            io.run();
+        });
+
         CLI cli(io, "127.0.0.1", 5555);
         cli.run();
-    } catch (const std::exception& e) {
-        std::cerr << e.what() << "\n";
+
+        // shutdown
+        io.stop();
+        ioThread.join();
+    }
+    catch (const std::exception& e) {
+        std::cerr << "[Client] error: " << e.what() << "\n";
+        return 1;
     }
 
-    // shutdown
-    clientWork.reset();
-    io.stop();
-    ioThread.join();
-
-    serverWork.reset();
-    serverIo.stop();
-    serverThread.join();
+    return 0;
 }
--- src/main_server.cpp
+++ src/main_server.cpp
@@ -1,5 +1,17 @@
-
+#include <asio.hpp>
+#include <iostream>
+#include "Network/TcpServer.h"
 
 int main() {
+    try {
+        asio::io_context io;
+        TcpServer server(io, 5555);
+        io.run();
+    }
+    catch (const std::exception& e) {
+        std::cerr << "[Server] error: " << e.what() << "\n";
+        return 1;
+    }
+
     return 0;
 }
